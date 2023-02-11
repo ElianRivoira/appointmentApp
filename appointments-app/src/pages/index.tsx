f188fd50e753adade7c28b1f445904fdf4586fea
ElianRivoira <eli_rivoira@hotmail.com>
2023-02-11 16:57:54 -0300

    login functionality

--- appointments-app/src/pages/index.tsx
+++ appointments-app/src/pages/index.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useRouter } from 'next/router';
+import { AppDispatch, RootState } from '@/store';
+import { fetchUser } from '@/store/slices/userSlice';
 
 const Index = () => {
-  return (
-    <div>index</div>
-  )
-}
+  const dispatch = useDispatch<AppDispatch>();
+  const state = useSelector((state: RootState) => state.user);
+  const router = useRouter();
 
-export default Index
+  useEffect(() => {
+    async function func() {
+      await dispatch(fetchUser());
+      console.log(state)
+      if (state.user) {
+        router.push('reservePanel');
+      } else {
+        router.push('login');
+      }
+    }
+    func();
+  }, []);
+
+  return <div>index</div>;
+};
+
+export default Index;
