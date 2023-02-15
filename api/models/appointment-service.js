2adb6ad23c861acf5090256344e705d4426a7f0c
ElianRivoira <eli_rivoira@hotmail.com>
2023-02-15 12:40:19 -0300

    added a generation of an id with 16 characteres

--- api/models/appointment-service.js
+++ api/models/appointment-service.js
@@ -1,12 +1,17 @@
-const { where } = require('./Appointment.model');
 const Appointment = require('./Appointment.model');
 
-const postReserve = async (data) => {
-  const appointment = await Appointment.create(data);
-  return appointment;
+const postReserve = async data => {
+  const ap = await Appointment.findOne();
+  if (ap) {
+    let aux = ap.id.split('-');
+    aux[0] = Number(aux[0]) + 1;
+    const id = aux.join('-');
+    const appointment = await Appointment.create({ ...data, id: id });
+    return appointment;
+  }
 };
 
-const getAllAppointmentsFromUser = async (userId) => {
+const getAllAppointmentsFromUser = async userId => {
   console.log('userId:', userId);
   const appointments = await Appointment.find(
     { userId },
