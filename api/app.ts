07600b88d5ea235360c87920f82bdd56e1074a67
ElianRivoira <eli_rivoira@hotmail.com>
2023-07-06 18:05:36 -0300

    cookie session samesite none

--- api/app.ts
+++ api/app.ts
@@ -29,7 +29,7 @@ app.use(
     maxAge: 24 * 60 * 60 * 1000, // Set the cookie to expire after 24 hours
     secure: false, // Set to true if using HTTPS
     httpOnly: false,
-    sameSite: 'strict',
+    sameSite: 'none',
   })
 );
 app.use(morgan('dev'));
