53288c085fe2455924b6bdd79185bcf15e1fb6fb
ElianRivoira <eli_rivoira@hotmail.com>
2023-07-10 11:50:36 -0300

    api url

--- appointments-app/src/services/axiosInstance.ts
+++ appointments-app/src/services/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_LOCALAPI,
+  baseURL: process.env.NEXT_PUBLIC_API,
   headers: {
     'Content-Type': 'application/json;charset=UTF-8',
   },
@@ -9,7 +9,7 @@ export const api = axios.create({
 });
 
 export const apiForm = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_LOCALAPI,
+  baseURL: process.env.NEXT_PUBLIC_API,
   headers: {
     'Content-Type': 'multipart/form-data',
   },
